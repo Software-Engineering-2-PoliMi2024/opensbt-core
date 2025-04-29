158cab248c3112b40ede9250c059b2d10ba7fe85
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-04-29 17:14:37 +0200

    chore: remove other examples and update requirements to include opensbt

--- examples/esmini/config.py
+++
@@ -1,2 +0,0 @@
-#ESMINI_PATH = "C:\\Program Files\\esmini-bin_Windows\\esmini\\bin\\esmini"
-ESMINI_PATH = r"C:\Users\sorokin\Documents\Projects\testing\opensbt\opensbt-core\examples\esmini\esmini\bin\esmini"
