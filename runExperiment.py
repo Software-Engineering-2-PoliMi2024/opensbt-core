68aea4f8bf3c541fe86c332f0e9e99c5c415fcb0
figinii <matteo5.figini@mail.polimi.it>
2025-08-12 12:27:44 +0200

    chore: add example scripts

--- runExperiment.py
+++ runExperiment.py
@@ -1,4 +1,13 @@
-from lanekeeping import UdacitySimulator, UdacitySimulatorConfig
+from experimentsRunner import ExperimentConfigParser
+from experimentsRunner import ExperimentRunner
+from dbInteract import MongoInteract
 
-config = UdacitySimulatorConfig()
-print(UdacitySimulator.simulate(simulator_config=config))
+DB_URI = 'mongodb+srv://matteo5figini:u1pJGtNXTqn5gTiP@se2db.tdibt7g.mongodb.net/?retryWrites=true&w=majority&appName=se2db'
+
+parser = ExperimentConfigParser()
+db = MongoInteract(DB_URI)
+expConfig = parser.parse("./sample.json")
+expManager = ExperimentRunner(expConfig, db)
+scenarioId, expTime = expManager.run()
+
+print(f"Experiment completed with ID: {scenarioId} in {expTime:.2f} seconds")
---
+++ runExperiment.sh
@@ -0,0 +1,1 @@
+xvfb-run python runExperiment.py
