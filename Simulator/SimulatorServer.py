d691f9abdac46efa2e51bee4a8d76cd27fa4a4f2
Paolo Ginefra <ginefra.paolo@gmail.com>
2025-08-15 18:51:41 +0200

    feat: add /health endpoint

--- Simulator/SimulatorServer.py
+++ Simulator/SimulatorServer.py
@@ -16,50 +16,58 @@ app = FastAPI()
 jobQueue = Queue()
 results = {}
 
+
 def simulationThread():
     simulator = UdacitySimulator()
-    
 
     while True:
-        #Fetches a job from the queue
+        # Fetches a job from the queue
         jobId, config = jobQueue.get(block=True)
 
         try:
-            #Performs the simulation
-            simOutput : SimulationOutput = simulator.simulate(simulator_config=config)
+            # Performs the simulation
+            simOutput: SimulationOutput = simulator.simulate(
+                simulator_config=config)
 
-            #Parse the output into a json
+            # Parse the output into a json
             output = jsonable_encoder(simOutput, custom_encoder={
-                    np.float32: float,
-                    np.float64: float
-                })
-            
-            #Add output to results
+                np.float32: float,
+                np.float64: float
+            })
+
+            # Add output to results
             results[jobId] = {"status": "done", "output": output}
         except Exception as e:
-            #Add error to the results
+            # Add error to the results
             results[jobId] = {"status": "error", "error": str(e)}
         finally:
             jobQueue.task_done()
 
+
 # Start simulator thread
 Thread(target=simulationThread, daemon=True).start()
 
+
 @app.post("/simulate")
 async def simulate(config: UdacitySimulatorConfig):
-    #Generate a unique jobId
+    # Generate a unique jobId
     jobId = str(uuid.uuid4())
 
-    #Set the status as queued
-    results[jobId] = {"status" : "queued"}
+    # Set the status as queued
+    results[jobId] = {"status": "queued"}
 
-    #Enqueues the job
+    # Enqueues the job
     jobQueue.put((jobId, config))
 
-    #Returns the job id
+    # Returns the job id
     return {'jobId': jobId}
 
 
 @app.get("/simulate/{job_id}")
 def get_result(job_id: str):
     return results.get(job_id, {"status": "not_found"})
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "Up and running"}
